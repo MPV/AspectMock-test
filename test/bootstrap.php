6ccc3755d4837f60552562ac285138ba3c0bb0e1
Victor Sollerhed <victor.sollerhed@ticnet.se>
2016-01-14 16:36:30 +0100

    Fixed this problem by using bootstrap file: Constant AOP_CACHE_DIR already defined

---
+++ test/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+
+include __DIR__.'/../vendor/autoload.php';
+
+$kernel = \AspectMock\Kernel::getInstance();
+$kernel->init([
+	'debug' => true,
+	'includePaths' => [
+		__DIR__.'../src/'
+	],
+	'excludePaths' => [
+		__DIR__.'../vendor', 
+		__DIR__
+	]
+]);
