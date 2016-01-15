17b37f58d2e1a2f70d9c06eb4153f1763659df45
Victor Sollerhed <victor.sollerhed@ticnet.se>
2016-01-15 17:31:42 +0100

    Moved autoloading into AspectMock kernel->loadFile() and skipped verifyInvoked.
    - I've now understood that AspectMock works via autoloading, and thus requires control over it, so now I'm asking the AspectMock kernel to load the files instead of including them manually.
    - Skipped "verifyInvoked" since it might not always work according to AspectMock Github issues.
    
    ...still no success yet.  :)

--- test/MyClassTest.php
+++ test/MyClassTest.php
@@ -1,33 +1,36 @@
 <?php
 
-include __DIR__.'/../vendor/autoload.php';
-include __DIR__.'/../src/MyClass.php';
+//include __DIR__.'/../vendor/autoload.php';
+//include __DIR__.'/../src/MyClass.php';
 
 class MyClassTest extends PHPUnit_Framework_TestCase{
 	
+	/*
 	public function testClassMethodWithoutMocking()
 	{
 		$this->assertEquals('always this string', MyClass::myClassMethod());
 	}
+	*/
 	
 	public function testMockingClassMethod()
 	{
 		$this->initAspectMock();
 		
-		$mock = \AspectMock\Test::double(MyClass::class, ['myClassMethod' => 'a']);
-		//$mock = \AspectMock\Test::double('MyClass', ['myClassMethod' => 'a']);
+		//$mock = \AspectMock\Test::double(MyClass::class, ['myClassMethod' => 'a']);
+		$mock = \AspectMock\Test::double('MyClass', ['myClassMethod' => 'a']);
 		//$mock = \AspectMock\Test::double('\MyClass', ['myClassMethod' => 'a']);
 		
 		$classMethodReturned = MyClass::myClassMethod();
 		
 		// Extra verification to see if the AspectMock double is actually being run yet:
-		$mock->verifyInvoked('myClassMethod');
+		//$mock->verifyInvoked('myClassMethod');
 		
 		$this->assertEquals('a', $classMethodReturned);
 	}
 	
 	protected function initAspectMock()
 	{
+		/** @var \AspectMock\Kernel $kernel */
 		$kernel = \AspectMock\Kernel::getInstance();
 		$kernel->init([
 			'debug' => true,
@@ -38,8 +41,11 @@ class MyClassTest extends PHPUnit_Framework_TestCase{
 			],
 			'excludePaths' => [
 				__DIR__,	// tests
+				//__DIR__.'../vendor/',
 			]
 		]);
+		$kernel->loadFile(__DIR__ . '/../vendor/autoload.php');
+		$kernel->loadFile(__DIR__ . '/../src/MyClass.php');
 	}
 	
 	/*
